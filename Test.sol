5ad21dcbfa8b32a73a641b2cf20a9e819207f526
Dheeraj K <132527159+Dheerajk1@users.noreply.github.com>
2024-09-30 18:30:56 +0530

    Update Test.sol

--- Test.sol
+++ Test.sol
@@ -5,37 +5,43 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20,Ownable,ERC20Burnable
+contract DegenToken is ERC20, Ownable, ERC20Burnable
 {
+    constructor() ERC20("Dheeraj", "DK") Ownable(msg.sender) {}
 
-    constructor() ERC20("Dheeraj", "DK") Ownable(msg.sender){}
-
-    function mint(address to, uint256 amount)public onlyOwner   // function for minting tokens. 
-    {
+    // Function for minting tokens
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    struct Entity                   // creating structure entity.
-    {
+
+    // Function for burning tokens
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+
+    // Function for transferring tokens
+    function transferTokens(address to, uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(msg.sender, to, amount);
+    }
+
+    struct Entity {
         string name;
         uint256 price;
     }
-    
 
-    mapping(uint256 => Entity) public features;         // done mapping to store available features
+    mapping(uint256 => Entity) public features;  // Storing available features
+    mapping(address => mapping(uint256 => bool)) public userFeatures;  // Tracking purchased features
 
-    mapping(address => mapping(uint256 => bool)) public userFeatures;  // done mapping to keep track of which features a user has bought
+    event FeaturePurchased(address indexed user, uint256 featureId, string featureName);
 
-
-    event FeaturePurchased(address indexed user, uint256 featureId, string featureName); // creating event, through this we will be purchasing features.
-
-
-    function addFeature(uint256 featureId, string memory name, uint256 price) public onlyOwner  // function to add features.
-    {
+    // Adding a new feature
+    function addFeature(uint256 featureId, string memory name, uint256 price) public onlyOwner {
         features[featureId] = Entity(name, price);
     }
 
-    function buyFeature(uint256 featureId) public    // function to buy features.
-    {
+    // Buying a feature
+    function buyFeature(uint256 featureId) public {
         Entity memory feature = features[featureId];
         require(bytes(feature.name).length != 0, "Feature does not exist");
         require(!userFeatures[msg.sender][featureId], "Feature already purchased");
@@ -48,10 +54,8 @@ contract DegenToken is ERC20,Ownable,ERC20Burnable
         emit FeaturePurchased(msg.sender, featureId, feature.name);
     }
 
-
-    function hasPurchasedFeature(address user, uint256 featureId) public view returns (bool)  // it will make sure that the feature is succesfully pyrchased or not.
-    {
+    // Check if a feature has been purchased
+    function hasPurchasedFeature(address user, uint256 featureId) public view returns (bool) {
         return userFeatures[user][featureId];
     }
-    
 }
